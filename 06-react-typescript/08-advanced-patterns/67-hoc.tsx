4dd39fc74cbd4ace19f75527cd279b41ac228015
Kobe Ruado <insidiae423@gmail.com>
2024-04-13 02:40:49 +0800

    Finish Advanced React Section 5

---
+++ 06-react-typescript/08-advanced-patterns/67-hoc.tsx
@@ -0,0 +1,31 @@
+import { Router, useRouter } from "fake-external-lib";
+
+export function withRouter<P>(Component: React.ComponentType<P>) {
+  const NewComponent = (props: Omit<P, "router">) => {
+    const router = useRouter();
+    return <Component {...(props as P)} router={router} />;
+  };
+
+  NewComponent.displayName = `withRouter(${Component.displayName})`;
+
+  return NewComponent;
+}
+
+const UnwrappedComponent = (props: { router: Router; id: string }) => {
+  return null;
+};
+
+const WrappedComponent = withRouter(UnwrappedComponent);
+
+<>
+  {/* @ts-expect-error needs a router! */}
+  <UnwrappedComponent id="123" />
+
+  {/* Doesn't need a router passed in! */}
+  <WrappedComponent id="123" />
+
+  <WrappedComponent
+    // @ts-expect-error id must be the correct property
+    id={123}
+  />
+</>;
---
+++ 06-react-typescript/08-advanced-patterns/72-as-prop-with-forward-ref.tsx
@@ -0,0 +1,167 @@
+import {
+  ComponentPropsWithRef,
+  ElementType,
+  ForwardedRef,
+  forwardRef,
+  useRef,
+} from "react";
+import { Equal, Expect } from "../helpers/type-utils";
+
+// Added fixedForwardRef from a previous exercise
+type FixedForwardRef = <T, P = {}>(
+  render: (props: P, ref: React.Ref<T>) => React.ReactNode
+) => (props: P & React.RefAttributes<T>) => React.ReactNode;
+
+const fixedForwardRef = forwardRef as FixedForwardRef;
+
+// Added a DistributiveOmit type
+type DistributiveOmit<T, TOmitted extends PropertyKey> = T extends any
+  ? Omit<T, TOmitted>
+  : never;
+
+/**
+ * INSTRUCTOR'S NOTE:
+ * FYI - this solution took me the best part of a whole day to find,
+ * and the help of several TS experts in the community.
+ *
+ * So, don't feel bad if you don't find it at all.
+ */
+// export function UnwrappedLink<AsType extends ElementType>(
+//   props: {
+//     as?: AsType;
+//   } & DistributiveOmit<
+//     ComponentPropsWithRef<ElementType extends AsType ? "a" : AsType>,
+//     "as"
+//   >,
+//   ref: ForwardedRef<any>
+// ) {
+//   const { as: Comp = "a", ...rest } = props;
+//   return <Comp {...rest} ref={ref}></Comp>;
+// }
+
+//? Again, for some reason the autocomplete still works with a default `as` prop now
+export function UnwrappedLink<AsType extends ElementType = "a">(
+  props: {
+    as?: AsType;
+  } & DistributiveOmit<ComponentPropsWithRef<AsType>, "as">,
+  ref: ForwardedRef<any>
+) {
+  const { as: Comp = "a", ...rest } = props;
+  return <Comp {...rest} ref={ref}></Comp>;
+}
+
+const Link = fixedForwardRef(UnwrappedLink);
+
+/**
+ * Should work without specifying 'as'
+ */
+
+const Example1 = () => {
+  const ref = useRef<HTMLAnchorElement>(null);
+  const wrongRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <Link ref={ref} />
+
+      <Link
+        // @ts-expect-error incorrect ref
+        ref={wrongRef}
+      />
+
+      <Link
+        // @ts-expect-error doesNotExist is not a valid prop
+        doesNotExist
+      ></Link>
+
+      <Link
+        // e should be inferred correctly
+        onClick={(e) => {
+          type test = Expect<
+            Equal<typeof e, React.MouseEvent<HTMLAnchorElement>>
+          >;
+        }}
+      ></Link>
+    </>
+  );
+};
+
+/**
+ * Should work specifying a 'button'
+ */
+
+const Example2 = () => {
+  const ref = useRef<HTMLButtonElement>(null);
+  const wrongRef = useRef<HTMLSpanElement>(null);
+
+  return (
+    <>
+      {/* CHECK ME! Check if autocomplete works on 'as' */}
+      <Link as="button" />
+
+      <Link as="button" ref={ref} />
+
+      <Link
+        as="button"
+        // @ts-expect-error incorrect ref
+        ref={wrongRef}
+      />
+
+      <Link
+        as="button"
+        // @ts-expect-error doesNotExist is not a valid prop
+        doesNotExist
+      ></Link>
+
+      <Link
+        as="button"
+        // e should be inferred correctly
+        onClick={(e) => {
+          type test = Expect<
+            Equal<typeof e, React.MouseEvent<HTMLButtonElement>>
+          >;
+        }}
+      ></Link>
+    </>
+  );
+};
+
+/**
+ * Should work with Custom components!
+ */
+
+const Custom = forwardRef(
+  (
+    props: { thisIsRequired: boolean },
+    ref: React.ForwardedRef<HTMLAnchorElement>
+  ) => {
+    return <a ref={ref} />;
+  }
+);
+
+const Example3 = () => {
+  const ref = useRef<HTMLAnchorElement>(null);
+  const wrongRef = useRef<HTMLDivElement>(null);
+  return (
+    <>
+      <Link as={Custom} thisIsRequired />
+      <Link
+        as={Custom}
+        // @ts-expect-error incorrectProp should not be allowed
+        incorrectProp
+      />
+
+      {/* @ts-expect-error thisIsRequired is not being passed */}
+      <Link as={Custom}></Link>
+
+      <Link as={Custom} ref={ref} thisIsRequired />
+
+      <Link
+        as={Custom}
+        // @ts-expect-error incorrect ref
+        ref={wrongRef}
+        thisIsRequired
+      />
+    </>
+  );
+};
